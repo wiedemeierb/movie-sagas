85c4dc8d74c77f1e3e60712774c6f8c9b6aa2066
Brandon Wiedemeier <brandon.wiedemeier@gmail.com>
2019-09-03 09:01:09 -0500

    edit page is set up

--- src/components/Edit/Edit.jsx
+++ src/components/Edit/Edit.jsx
@@ -2,9 +2,19 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Edit extends Component {
+
+    handleClickCancel = (id) => {
+      console.log('handleClickCancel is working')
+      this.props.history.push('/details');
+    }
     render() {
         return (
+            <>
             <h2>Edit Page</h2>
+            <input />
+            <button onClick={this.handleClickCancel}>Cancel</button>
+            <button>Save</button>
+            </>
         )
     }
 }
